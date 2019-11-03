99cd08604d46538ac2666dc883d00f2b01226bab
TacticalCamper <Niklas.Limberg@alieris.de>
2019-11-03 12:14:00 +0100

    designed Login

--- pages/login.vue
+++ pages/login.vue
@@ -1,54 +1,153 @@
 <template>
-    <div>
-        <div>
-            <label for="email">E-Mail</label>
-            <input id="email" v-model="email">
-        </div>
-        <div>
-            <label for="password">Password</label>
-            <input id="password" v-model="password">
-        </div>
-        <div>
-            <button @click="login">Login</button>
-        </div>
-    </div>
+  <div class="center">
+    <main class="form-container">
+      <h3>Log in to your account</h3>
+      <div class="form-group">
+        <font-awesome-icon icon="user" />
+        <label for="email">E-Mail</label>
+        <input
+          id="email"
+          class="form-control"
+          placeholder="Email Adress"
+          type="email"
+          v-model="email"
+          v-on:keyup.enter="login"
+          tabindex="0"
+        />
+      </div>
+      <div class="form-group">
+        <font-awesome-icon icon="lock" />
+        <label for="password">Password</label>
+        <input
+          id="password"
+          class="form-control"
+          placeholder="Password"
+          type="password"
+          v-model="password"
+          v-on:keyup.enter="login"
+          tabindex="0"
+        />
+      </div>
+      <div v-if="loginError">{{loginError}}</div>
+      <div>
+        <button @click="login" tabindex="0">Login In</button>
+      </div>
+    </main>
+  </div>
 </template>
 
 <script>
-    import Show from "~/models/Show";
-    import Episode from "~/models/Episode";
+import Show from "~/models/Show";
+import Episode from "~/models/Episode";
 
-    export default {
-        name: "login",
-        template: "login",
-        data: function () {
-            return {
-                email: "",
-                password: ""
-            }
-        },
-        methods: {
-            login() {
-                console.log(process.env.PASSPORT_PASSWORD_GRANT_ID);
-                this.$auth.loginWith('password_grant', {
-                    data: {
-                        grant_type: 'password',
-                        client_id: process.env.PASSPORT_PASSWORD_GRANT_ID + "",
-                        client_secret: process.env.PASSPORT_PASSWORD_GRANT_SECRET,
-                        scope: '*',
-                        username: this.email,
-                        password: this.password
-                    }
-                }).then(async res => {
-                    Show.create({data: await this.$axios.$get('/api/shows')});
-                    Episode.create({data: await this.$axios.$get('/api/episodes')});
-                })
-                    .catch(err => console.log(err));
-            }
-        }
+export default {
+  name: "login",
+  layout: "login",
+  data: function() {
+    return {
+      email: "",
+      password: "",
+      loginError: undefined
+    };
+  },
+  head() {
+    return {
+      title: "Login"
+    };
+  },
+  methods: {
+    login() {
+      const validationMsg = document.getElementById("email").validationMessage;
+      if (validationMsg !== "") {
+        this.loginError = validationMsg;
+        return;
+      }
+      if (this.password === "") {
+        this.loginError = "Password is requierd";
+        return;
+      }
+      this.$auth
+        .loginWith("password_grant", {
+          data: {
+            grant_type: "password",
+            client_id: process.env.PASSPORT_PASSWORD_GRANT_ID,
+            client_secret: process.env.PASSPORT_PASSWORD_GRANT_SECRET,
+            scope: "*",
+            username: this.email,
+            password: this.password
+          }
+        })
+        .then(async res => {
+          const [shows, episodes] = await Promise.all([
+            this.$axios.$get("/api/shows"),
+            this.$axios.$get("/api/episodes")
+          ]);
+          Show.create({ data: shows });
+          Episode.create({ data: episodes });
+          await this.$store.commit("updateTimestamp");
+        })
+        .catch(err => {
+          this.loginError = err.response.data.error_description;
+        });
     }
+  }
+};
 </script>
 
 <style scoped>
+.center {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background: var(--btn-background);
+}
 
+h3 {
+  text-align: center;
+  line-height: 1.5;
+  font-size: 24px;
+  font-weight: 200;
+  color: var(--btn-background);
+}
+
+.form-container {
+  background: white;
+  padding: 40px;
+  border-radius: 8px;
+  width: 25vw;
+}
+
+.form-group {
+  display: flex;
+  align-items: baseline;
+  font-size: 14px;
+  border: 1px solid #cbcbd2;
+  padding: 8px;
+  margin-bottom: 20px;
+  width: 100%;
+  box-sizing: border-box;
+  border-radius: 4px;
+}
+.form-control {
+  height: 2rem;
+  border: none;
+  width: auto;
+  margin-left: 6px;
+  flex-grow: 1;
+}
+
+button {
+  width: 100%;
+}
+label {
+  position: absolute;
+  overflow: hidden;
+  clip: rect(0 0 0 0);
+  height: 1px;
+  width: 1px;
+  margin: -1px;
+  padding: 0;
+  border: 0;
+}
 </style>
--- store/index.js
+++ store/index.js
@@ -12,10 +12,18 @@ import Show from "~/models/Show";
 import Episode from "~/models/Episode";
 
 export const actions = {
-    async nuxtServerInit(store, {$auth}) {
+    async nuxtServerInit(store, {$auth, $axios}) {
         if ($auth.loggedIn) {
             Show.create({data: await this.$axios.$get('/api/shows')});
             Episode.create({data: await this.$axios.$get('/api/episodes')});
+            store.state.lastUpdated = Math.floor(Date.now() / 1000);
         }
     }
 };
+
+
+export const mutations  = {
+    updateTimestamp (state) {
+        state.lastUpdated = Math.floor(Date.now() / 1000);
+    }
+}
