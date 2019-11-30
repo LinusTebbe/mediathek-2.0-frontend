a9bcf670bfe67d3f7f7a6a2891375a20cb126456
TacticalCamper <Niklas.Limberg@alieris.de>
2019-11-30 16:57:39 +0100

    added sidebar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,18 +1,98 @@
 <template>
-    <div>
-        <nuxt/>
+  <div>
+    <nav>
+      <div>
+        <font-awesome-icon v-if="canGoBack" @click="back()" class="back" icon="arrow-left" />
+      </div>
+      <span class="title">
+        <b>Mediathek</b>.alieris.de
+      </span>
+      <font-awesome-icon @click="SidebarVisible = !SidebarVisible" icon="bars" />
+    </nav>
+    <div class="sidebar" :class="[SidebarVisible ? 'show' : 'hide']">
+      <div class="sidebar-element">
+        <div>Eingelogt als: {{this.$auth.user.name}}</div>
+        <button @click="logout()">Logout</button>
+      </div>
     </div>
+    <nuxt />
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "default",
-        mounted() {
-            this.$axios.$get()
-        }
+export default {
+  name: "default",
+  data: function() {
+    return {
+      SidebarVisible: false
+    };
+  },
+  computed: {
+    canGoBack: function() {
+      return this.$route.name !== "index";
     }
+  },
+  methods: {
+    back: () => window.history.back(),
+    logout: async function() {
+      await this.$auth.logout();
+      this.$router.go({ name: "login" });
+    }
+  },
+  async mounted() {
+    this.$store.commit("updateTimestamp");
+  }
+};
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+nav {
+  display: grid;
+  align-items: center;
+  grid-template-columns: 3rem 1fr 3rem;
 
+  position: sticky;
+  top: 0;
+  z-index: 5;
+  height: 3rem;
+
+  background: var(--nav-background-color);
+  color: var(--text-secondry-color);
+  font-size: 2rem;
+}
+
+.title {
+  margin-top: 0.1rem;
+}
+.back {
+  cursor: pointer;
+}
+
+.sidebar {
+  position: fixed;
+  top: 3rem;
+  left: -300px;
+  background: white;
+  z-index: 5;
+  padding: 1rem;
+  height: 100vh;
+  width: 20vw;
+}
+.sidebar.show {
+  left: 0px;
+  transition: all 0.5s;
+}
+.sidebar.hide {
+  left: -300px;
+  transition: all 0.5s;
+}
+
+.sidebar-element {
+  div {
+    margin: .5rem 0;
+  }
+  button {
+    width: 100%;
+  }
+}
 </style>
