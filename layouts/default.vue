6a99c97abc57b7295757f0eb597826a8e89e8d2e
Niklas Limberg <faultierchenchen@gmail.com>
2019-12-14 15:54:05 +0100

    fix styles

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
     <nav>
-      <div>
-        <font-awesome-icon v-if="canGoBack" @click="back()" class="back" icon="arrow-left" />
-      </div>
+      <font-awesome-icon class="back-icon" v-if="canGoBack" @click="back()" icon="arrow-left" />
       <span class="title">
         <b>Mediathek</b>.alieris.de
       </span>
-      <font-awesome-icon style="cursor: pointer" @click="SidebarVisible = !SidebarVisible" icon="bars" />
+      <font-awesome-icon
+        class="sidebar-toggle"
+        @click="SidebarVisible = !SidebarVisible"
+        icon="bars"
+      />
     </nav>
     <div class="sidebar" v-bind:class="{ show: SidebarVisible   }">
       <div class="sidebar-element">
@@ -44,49 +46,55 @@ export default {
       await this.$auth.logout();
       this.$router.go({ name: "login" });
     },
-    search : function () {
+    search: function() {
       this.$router.push({ name: "search", query: { s: this.searchTerm } });
     }
   },
   async mounted() {
-    this.$store.commit("updateTimestamp");
+    this.$store.commit("updateData");
   }
 };
 </script>
 
 <style scoped lang="scss">
 nav {
-  display: grid;
+  .back-icon {
+    margin-left: 5px;
+    cursor: pointer;
+  }
+  .title {
+    margin-left: 5px;
+  }
+  .sidebar-toggle {
+    cursor: pointer;
+    position: absolute;
+    top: 13px;
+    right: 5px;
+  }
+
   align-items: center;
-  grid-template-columns: 3rem 1fr 3rem;
 
   position: sticky;
   top: 0;
   z-index: 5;
-  height: 3rem;
+  padding: 5px 0;
 
   background: var(--nav-background-color);
   color: var(--text-secondry-color);
   font-size: 2rem;
 }
 
-.title {
-  margin-top: 0.1rem;
-}
-.back {
-  cursor: pointer;
-}
-
 .sidebar {
   width: 25rem;
   position: fixed;
-  top: 3rem;
-  left: -26rem;
+  top: 54px;
+  left: -25rem;
   background: white;
   z-index: 5;
   padding: 1rem;
   height: 100vh;
   transition: all 0.5s;
+  box-sizing: border-box;
 }
 .sidebar.show {
   left: 0;
