576afdb084a9cf5f9b72a416339f1a2e6d9cb713
Niklas Limberg <faultierchenchen@gmail.com>
2019-12-18 18:00:14 +0100

    insert Episodes and Shows to not overwite old data

--- store/index.js
+++ store/index.js
@@ -1,18 +1,12 @@
-//import VuexORMisDirtyPlugin from '@vuex-orm/plugin-change-flags';
 import VuexORMSearch from '@vuex-orm/plugin-search'
 import VuexORM from '@vuex-orm/core'
 import database from '~/database'
 
-//VuexORM.use(VuexORMisDirtyPlugin);
-VuexORM.use(VuexORMSearch, {
-    // Configure default fuse.js options here (see "Configuration" section below).
-})
-
+VuexORM.use(VuexORMSearch);
 
 export const plugins = [
     VuexORM.install(database)
 ];
-
 
 import Show from "~/models/Show";
 import Episode from "~/models/Episode";
@@ -20,9 +14,8 @@ import Episode from "~/models/Episode";
 export const actions = {
     async nuxtServerInit(store, { $auth, $axios }) {
         if ($auth.loggedIn) {
-            Show.create({ data: await this.$axios.$get('/api/shows') });
-            Episode.create({ data: await this.$axios.$get('/api/episodes') });
-            this.$store.commit("updateTimestamp");
+            Show.insert({ data: await this.$axios.$get('/api/shows') });
+            Episode.insert({ data: await this.$axios.$get('/api/episodes') });
         }
     }
 };
